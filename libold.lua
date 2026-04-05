b267756b94444d547401e1019f4e6d43e1f956ba
kwi <zerokoolaid1.0@gmail.com>
2026-04-05 03:37:47 -0700

    Create libold.lua

---
+++ libold.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kwipflyx/new/refs/heads/main/lib.lua'))()
+queue_on_teleport([[
+task.wait(5)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kwipflyx/new/refs/heads/main/lib.lua'))()
+]])
